1f5baa89fa67939d23c8ebd405a6626bf2f2a22f
Kaue ARON <kauearon@gmail.com>
2025-10-27 02:45:55 -0300

    feat(ui): Adicionando paleta de cores Brand e Neutral do Figma

--- app/src/main/java/com/example/guardia/ui/theme/Color.kt
+++ app/src/main/java/com/example/guardia/ui/theme/Color.kt
@@ -2,10 +2,28 @@ package com.example.guardia.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+//TONS DE AZUL(BRAND DO FIGMA)
+val brand50 = Color(0xFFEBF8FF)
+val brand100 = Color(0xFFDBEAFE)
+val brand200 = Color(0xFFBFDBFE)
+val brand300 = Color(0xFF93C5FD)
+val brand400 = Color(0xFF60A5FA)
+val brand500 = Color(0xFF3B82F6)
+val brand600 = Color(0xFF2563EB)
+val brand700 = Color(0xFF1D4ED8)
+val brand800 = Color(0xFF1E40AF)
+val brand900 = Color(0xFF1E3A8A)
+
+//TONS NEUTROS(NEUTRAL DO FIGMA)
+
+val neutral50 = Color(0xFFF9FAFB)
+val neutral100 = Color(0xFFF3F4F6)
+val neutral200 = Color(0xFFE5E7EB)
+val neutral300 = Color(0xFFD1D5DB)
+val neutral400 = Color(0xFF9CA3AF)
+val neutral500 = Color(0xFF6B7280)
+val neutral600 = Color(0xFF4B5563)
+val neutral700 = Color(0xFF374151)
+val neutral800 = Color(0xFF1F2937)
+val neutal900 = Color(0xFF111827)
